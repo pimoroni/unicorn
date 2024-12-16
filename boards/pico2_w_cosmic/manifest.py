07d6af405bc174fe3683dfa0a9f5f2cda7a2ec54
Phil Howard <github@gadgetoid.com>
2024-12-16 13:03:48 +0000

    Add Cosmic and Stellar Pico 2 builds.

---
+++ boards/pico2_w_cosmic/manifest.py
@@ -0,0 +1,2 @@
+include("$(PORT_DIR)/boards/manifest.py")
+include("../manifest-common.py")
---
+++ boards/pico2_w_cosmic/mpconfigboard.h
@@ -0,0 +1,14 @@
+// Board and hardware specific configuration
+#define MICROPY_HW_BOARD_NAME                   "Raspberry Pi Pico2 W (Cosmic Unicorn)"
+
+// Leave 50/50 split - 2MB firmware, 2MB user filesystem
+#define MICROPY_HW_FLASH_STORAGE_BYTES          (PICO_FLASH_SIZE_BYTES - (2 * 1024 * 1024))
+
+// Enable networking.
+#define MICROPY_PY_NETWORK_PPP_LWIP             (1)
+#define MICROPY_PY_NETWORK_HOSTNAME_DEFAULT     "CosmicUnicorn2W"
+#include "enable_cyw43.h"
+
+// For debugging mbedtls - also set
+// Debug level (0-4) 1=warning, 2=info, 3=debug, 4=verbose
+// #define MODUSSL_MBEDTLS_DEBUG_LEVEL 1
