5b39893fed5e8200fb1e1da21162a6867ecbd7d0
Phil Howard <github@gadgetoid.com>
2025-04-29 15:30:32 +0100

    Pico W Unicorns: Drop some modules to make the build fit.

--- boards/usermod-common.cmake
+++ boards/usermod-common.cmake
@@ -30,7 +30,36 @@ include(jpegdec/micropython)
 include(qrcode/micropython/micropython)
 
 # Sensors & Breakouts
-include(micropython-common-breakouts)
+
+# Include in Pico 2 W only, these make the build too big for Pico W :(
+# Do you really need to connect a matrix to your matrix!?
+if(PICO_BOARD EQUAL "pico2_w")
+include(breakout_dotmatrix/micropython)
+include(breakout_rgbmatrix5x5/micropython)
+include(breakout_matrix11x7/micropython)
+endif()
+
+include(breakout_encoder/micropython)
+include(breakout_encoder_wheel/micropython)
+include(breakout_ioexpander/micropython)
+include(breakout_ltr559/micropython)
+include(breakout_as7262/micropython)
+include(breakout_as7343/micropython)
+include(breakout_msa301/micropython)
+include(breakout_pmw3901/micropython)
+include(breakout_mics6814/micropython)
+include(breakout_potentiometer/micropython)
+include(breakout_rtc/micropython)
+include(breakout_trackball/micropython)
+include(breakout_sgp30/micropython)
+include(breakout_bh1745/micropython)
+include(breakout_bme69x/micropython)
+include(breakout_bme68x/micropython)
+include(breakout_bme280/micropython)
+include(breakout_bmp280/micropython)
+include(breakout_icp10125/micropython)
+include(breakout_scd41/micropython)
+include(breakout_vl53l5cx/micropython)
 
 # Utility
 include(adcfft/micropython)
